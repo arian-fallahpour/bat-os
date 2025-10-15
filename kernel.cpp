d235b970b5d597e993c10f0292499d2cc6561132
JinXzAF <arianf2004@gmail.com>
2025-10-14 22:29:05 -0400

    Initial commit

---
+++ kernel.cpp
@@ -0,0 +1,23 @@
+
+void printf(char* str) {
+    unsigned short* VideoMemory = (unsigned short*)0xb8000; // Video memory begins at address 0xb8000
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i]; // OxFF00 black background, white text
+    }
+}
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors; 
+extern "C" constructor end_ctors;
+extern "C" void callConstructors() {
+    for (constructor* i = &start_ctors; i != &end_ctors; i++) {
+        (*i)();
+    }
+} 
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) {
+    printf("Hello World!\n");  
+
+    while(1);
+};
